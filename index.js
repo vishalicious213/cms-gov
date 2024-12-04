beacf29609ccefb19728261fbf964214ec5bd7bc
vishalicious213 <vishalicious213@gmail.com>
2024-12-04 13:09:21 -0800

    created section with select element and cms-671 option. added event listener and event handler to log the value of the ID that's clicked so that the right tool can be rendered

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("LTC Info")
+const selectionArea = document.getElementById("selection-area")
+
+// ⬇️ EVENT LISTENERS ⬇️
+
+selectionArea.addEventListener("click", function(e) {
+    if (e.target.id) {
+        selectSelectionArea(e.target.value)
+    }
+})
+
+// ⬇️ EVENT HANDLERS ⬇️
+
+function selectSelectionArea(value) {
+    console.log(value)
+}
+
+// ⬇️ RENDER FUNCTIONS ⬇️
