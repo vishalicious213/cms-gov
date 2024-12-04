c1aa717b2c3d95bc264500428856284d96defc29
vishalicious213 <vishalicious213@gmail.com>
2024-12-04 15:15:51 -0800

    getting the states objects and converting them into an array. tried to add them as options to the #state drop-down, but its not working...

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mainArea = document.getElementById("main")
 const selectionArea = document.getElementById("selection-area")
 const selectionOther = document.getElementById("selection-other")
+let states = []
 
 // ⬇️ EVENT LISTENERS ⬇️
 
@@ -20,19 +21,38 @@ function selectSelectionArea(value) {
 
 function selectState() {
     selectionOther.innerHTML = ""
+    getStates()
 
     selectionOther.innerHTML = `
         <label class="label" for="state">Choose a state:</label>
         <select name="state" id="state">
             <option value="none"></option>
-            <option value="AK">AK</option>
         </select>
     `
+
+    const stateOptions = document.getElementById("state")
+    states.map (state => {
+        stateOptions += `<option value="${state}">${state}</option>`
+    })
+}
+
+// ⬇️ UTILITY FUNCTIONS ⬇️
+
+async function getStates() {
+    try {
+        const res = await fetch("https://data.cms.gov/data-api/v1/dataset/510f8762-0cf7-4aa3-93ff-e13af0b3bf26/data?column=State&additionalProp1=%7B%7D&offset=0&size=100&distinct=1")
+        const data = await res.json()
+        states = data.map(item => item.State)
+        console.log(states)
+    }
+    catch (err) {
+        console.error("Failed to fetch states from CMS:", err)
+    }
 }
 
 // ⬇️ RENDER FUNCTIONS ⬇️
 
-function renderMain() {
+function render671() {
     mainArea.innerHTML = ""
 
     mainArea.innerHTML = `
