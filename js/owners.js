9a3c3262869b26894a55177391325060c1f14316
vishalicious213 <vishalicious213@gmail.com>
2024-12-11 18:05:53 -0800

    owners.js: grabbed html sections and declared states[] & facilities[]

--- js/owners.js
+++ js/owners.js
@@ -1,1 +1,6 @@
-console.log("OWNERS")
+const mainArea = document.getElementById("main")
+const selectionArea = document.getElementById("selection-area")
+const selectionOther = document.getElementById("selection-other")
+let states = []
+let facilities = []
+
